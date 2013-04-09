8a0159f9fc0ebab822733fe2a357a30bbacc4c44
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-09 09:18:58 -0400

    added project model

--- crowdfunder/db/schema.rb
+++ crowdfunder/db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409130544) do
+ActiveRecord::Schema.define(:version => 20130409131630) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.string   "teaser"
+    t.integer  "user_id"
+    t.text     "description"
+    t.integer  "goal"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",            :null => false
