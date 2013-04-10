b9a44f3e2ce423ed027bbb0593d99a19f8b304d1
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-10 08:52:31 -0400

    added users controller, view and route

--- crowdfunder/config/routes.rb
+++ crowdfunder/config/routes.rb
@@ -5,7 +5,8 @@ Crowdfunder::Application.routes.draw do
   # first created -> highest priority.
 
 
-  resources :projects 
+  resources :projects
+  resources :users, :except =>[:index, :destroy]
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
