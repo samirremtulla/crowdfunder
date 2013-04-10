abd0d4f4e5af52bdab18b683218ccdc96bfc6dde
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-10 09:23:39 -0400

    updated error messages on sign-up form and testing

--- crowdfunder/app/models/user.rb
+++ crowdfunder/app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   # attr_accessible :title, :body
   has_many :projects
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true, :on => :create
 end
---
+++ crowdfunder/test/integration/user_authentication_flow_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class UserAuthenticationFlowTest < ActionDispatch::IntegrationTest
+
+  test "successful registration" do 
+    visit "/users/new"
+    assert_equal new_user_path, current_path
+
+    # The signup button should be visible unless already signed in
+    assert find('.navbar').has_link?('Sign Up')
+
+    user = FactoryGirl.build(:user) 
+
+    fill_in "user[email]", :with => user.email
+    fill_in "user[first_name]", :with => user.first_name
+    fill_in "user[last_name]", :with => user.last_name
+    fill_in "user[password]", :with => user.password
+    click_button "Create Account"
+
+    assert_equal root_path, current_path
+    assert page.has_content?("Account created")
+
+    # No sign up button, logout button instead
+    assert find('.navbar').has_no_link?('Sign Up')
+    assert find('.navbar').has_link?('Logout')
+  end
+
+  test "failed registration" do 
+    visit "/users/new"
+    user = FactoryGirl.build(:user) 
+
+    fill_in "user[email]", :with => user.email
+    click_button "Create Account"
+    assert_equal users_path, current_path
+    assert page.has_no_content?("Account created")
+
+    # Should see some sort of error message on failure to register 
+    assert find('.alert:first').has_content?("Try again")
+  end
+
+  test "logout" do 
+  end
+
+  test "log in" do 
+  end
+
+end
